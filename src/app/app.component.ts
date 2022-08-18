b2f6a6d3bd11f13ddec0c018dc9dc0ce605724d4
jarioss <armando.rios@udea.edu.co>
2022-08-18 17:24:45 -0500

    Eventos de tipo click, scroll y change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent { /*los objetos deben tener acceso publico para traerlo desde otro componente*/
   name = 'Jorge';
   age = 26;
-  img = "./imagenes/pexels-akın-akdağ-10953441.jpg";
+  img = 'https://source.unsplash.com/random';
+  btnDisable = true;
+  /*Vamos a crear un objeto en ts */
+  person = {
+    name: 'Jorge',
+    age: 26,
+    avatar: 'https://source.unsplash.com/random'
+  }
 
+  toggleButton() {
+    /*this.btnDisable = false; ==>>  esta funcion solo habilita si queremos habilitarlo o desabilitarlo se debe poner el estado negado, con esto si esta en true se llevara a false y viceversa*/
+    this.btnDisable =!this.btnDisable; /* Esto habilita o dessabilita de acuerdo al evento utilizado */
+  }
+  increaseAge(){
+    this.person.age += 1;
+  }
+  diminishAge(){
+    this.person.age -= 1;
+  }
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
+
+
