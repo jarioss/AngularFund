79216ca287755850d35f1868104ebf41daa5095a
jarioss <armando.rios@udea.edu.co>
2022-08-18 22:37:00 -0500

    Estructuras de control y estilos a la lista de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({ /*Esto es un decorador y tiene la funcion de como se va a comportar las clases contenidas aqui mismo*/
   selector: 'app-root',
@@ -16,6 +17,43 @@ export class AppComponent { /*los objetos deben tener acceso publico para traerl
     age: 26,
     avatar: 'https://source.unsplash.com/random'
   }
+  names : string[] = [];
+  newName = "";
+
+products: Product[] = [
+  {
+    name: "El mejor juguete",
+    price: 120,
+    image: './assets/images/pexels-carolina-castilla-arias-1716861.jpg',
+    category: '',
+  },
+  {
+    name: "Iphon 13 pro max como nuevo",
+    price: 1980,
+    image: './assets/images/pexels-caleb-oquendo-9667337.jpg'
+  },
+  {
+    name: "Balón futbol n°6",
+    price: 95,
+    image: './assets/images/pexels-pixabay-47730.jpg'
+  },
+  {
+    name: "Trilogía del Señor de los Anillos",
+    price: 250,
+    image: './assets/images/pexels-vincent-ma-janssen-6958119.jpg'
+  },
+  {
+    name: "Bicicleta  nueva marco 17, rin 29",
+    price: 2500,
+    image: './assets/images/pexels-mídia-3642534.jpg'
+  },
+  {
+    name: "Muchila Roja-azul",
+    price: 130,
+    image: './assets/images/pexels-gursharndeep-singh-1262692.jpg'
+  },
+
+]
 
   toggleButton() {
     /*this.btnDisable = false; ==>>  esta funcion solo habilita si queremos habilitarlo o desabilitarlo se debe poner el estado negado, con esto si esta en true se llevara a false y viceversa*/
@@ -34,6 +72,13 @@ export class AppComponent { /*los objetos deben tener acceso publico para traerl
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+  addName() {
+    this.names.push(this.newName);
+    this.newName ="";
+  }
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
 
